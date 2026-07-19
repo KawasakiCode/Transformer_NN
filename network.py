577d3d667516216d73c9eaef5be1d73f20a1126c
KawasakiCode <remvosp@gmail.com>
2026-07-19 14:57:07 +0300

    wrote the base of the network and the mlp's that will be used

--- network.py
+++ network.py
@@ -1,2 +1,25 @@
 import torch.nn as nn
 import torch 
+from torch.nn import functional as F
+
+class Transformer(nn.Module):
+    def __init__(self, vocab_size, n_embd, block_size):
+        super().__init__()
+
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+
+    def forward(self, idx, targets=None):
+        pass
+
+class MLP(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd)
+        )
+    
+    def forward(self, x):
+        return self.net(x)
