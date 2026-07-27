0715b0b5a8072fb51acae29721a6a8f4a50ef02b
KawasakiCode <remvosp@gmail.com>
2026-07-27 18:16:18 +0300

    testing new model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from network import Transformer
-from data import generate_data, generate_tinystories_dataset, get_batch
+from data import generate_tinystories_dataset, get_batch
 import torch
 from tqdm import tqdm
 
@@ -38,10 +38,10 @@ if __name__ == "__main__":
     block_size = 256
     batch_size = 64
 
-    n_embd = 320
+    n_embd = 512
     n_head = 16
     head_size = 32
-    num_blocks = 10
+    num_blocks = 12
 
     model = Transformer(vocab_size=vocab_size, block_size=block_size, n_embd=n_embd, num_blocks=num_blocks, n_head=n_head, head_size=head_size)
     model.to('cuda' if torch.cuda.is_available() else 'cpu')
@@ -53,7 +53,9 @@ if __name__ == "__main__":
 
     scaler = torch.amp.GradScaler('cuda')
 
-    max_iters = 5001
+    max_iters = 15001
+    prev_val_loss = 10
+
     for iter in tqdm(range(max_iters)):
         x, y = get_batch('train', train_data, test_data, block_size, batch_size)
 
@@ -68,6 +70,11 @@ if __name__ == "__main__":
 
         if iter % 1000 == 0:
             losses = estimate_loss(train_data, test_data, model, block_size, batch_size)
+            if losses['val'] > prev_val_loss:
+                print(f"Early stop step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+                break
+            prev_val_loss = losses['val']
+
             print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
     torch.save(model.state_dict(), 'transformer_weights.pth')
